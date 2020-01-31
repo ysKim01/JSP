858d78ede8006f7518a8f1745bea717fa28026b5
young <sey4848@gmail.com>
2020-01-31 21:47:42 +0900

    20200131_kgit 2

---
+++ pro02/src/InputServlet2.java
@@ -0,0 +1,34 @@
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/input2")
+public class InputServlet2 extends HttpServlet {
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init ½ÇÇà");
+	}
+	public void destroy() {
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		Enumeration enu = request.getAttributeNames();
+		while(enu.hasMoreElements()) {
+			String name = (String)enu.nextElement();
+			String[] values = request.getParameterValues(name);
+			for(String val : values) {
+				System.out.println("name = " + name + ", value = " + val);
+			}
+		}
+	}
+
+}
